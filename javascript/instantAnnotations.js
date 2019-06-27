f16b6a85c9f487a7137a6932d624b577ce2b7a98
Philipp Häusle <philipp.haeusle@hotmail.com>
2019-06-27 17:24:12 +0200

    fix for semantify function syntaxHighlighting

--- javascript/instantAnnotations.js
+++ javascript/instantAnnotations.js
@@ -1362,7 +1362,7 @@ this.IA_Init = function(settings) {
 
     function syntaxHighlight(json) {
         if(typeof json === 'object'){
-          json=JSON.stringify(json);
+          json=JSON.stringify(json, null, 2);
         }
         json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
         return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
